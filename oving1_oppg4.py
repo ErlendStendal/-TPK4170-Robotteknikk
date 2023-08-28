b83e0914c09dc7519027080e431fa239f2521a03
erlendstendal <erlendstendal@live.no>
2023-08-28 11:26:01 +0200

    gjorde oppg4

---
+++ oving1_oppg4.py
@@ -0,0 +1,11 @@
+import math
+
+def grubler(m, N, J, dof_i):
+    
+    dof = m*(N-1-J)
+    for i in dof_i:
+        dof += i    
+    
+    return dof
+
+print(grubler(3, 4, 4, [1, 1, 1, 1])) #out: 1
